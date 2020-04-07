28aeaf5fd44ab29262640d6ac0a1ffe4f0312e53
Felipe Vieira Baehr <felipebaehr@gmail.com>
2020-04-07 16:41:56 -0300

    Ajustes

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -1,12 +1,14 @@
 const pg = require('pg');
+const { environment } = require('../environments/environment.prod');
+
 const config = {
-  host: 'ec2-52-86-33-50.compute-1.amazonaws.com',
+  host: environment.host,
   // Do not hard code your username and password.
   // Consider using Node environment variables.
-  user: 'eyeitgwnzvsvym',
-  password: 'e5dc2daa99a5c90a56cc2e4fdb8b440b0a12dbac081d1248b87ccc353e5dc449',
-  database: 'd17lpl5npi2hj9',
-  port: 5432,
+  user: environment.user,
+  password: environment.password,
+  database: environment.database,
+  port: environment.port,
   ssl: true
 };
 
