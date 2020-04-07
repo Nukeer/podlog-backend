7ae92a56a7ae9cde7001a86daeac064e324ac037
Felipe Vieira Baehr <felipebaehr@gmail.com>
2020-04-07 16:33:57 -0300

    Feito variaveis de ambiente para armazenar os dados de conexão do banco

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  host: '',
+  user: '',
+  password: '',
+  database: '',
+  port: '',
+  ssl: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+// value default
+export const environment = {
+  production: true,
+  host: '',
+  user: '',
+  password: '',
+  database: '',
+  port: '',
+  ssl: true
+};
